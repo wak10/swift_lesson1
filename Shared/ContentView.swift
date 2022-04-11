bcc8b557e50033c75f63174ea5927ed91b58e904
wak10 <chiba.r25.taka@gmail.com>
2022-04-11 22:28:29 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Takahiro Chiba on 2022/04/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/swift_lesson1App.swift
@@ -0,0 +1,17 @@
+//
+//  swift_lesson1App.swift
+//  Shared
+//
+//  Created by Takahiro Chiba on 2022/04/11.
+//
+
+import SwiftUI
+
+@main
+struct swift_lesson1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
