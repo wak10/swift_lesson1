1630f7ff22b7303abc5ffe9dd4f225aeec4d0ec2
wak10 <chiba.r25.taka@gmail.com>
2022-04-15 23:11:41 +0900

    [update]レイアウト調整

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -12,26 +12,33 @@ struct ContentView: View {
     @State var answerNumber = 0
     var body: some View {
         VStack {
-            
+            Spacer()
             if answerNumber == 0 {
                 Text("これからじゃんけんをします")
+                    .padding(.bottom)
             }else if answerNumber == 1 {
                 Image("gu")
                     //リサイズを指定
                     .resizable()
                     //アスペクト比を修正
                     .aspectRatio(contentMode: .fit)
+                Spacer()
                 Text("グー")
+                    .padding(.bottom)
             }else if answerNumber == 2 {
                 Image("choki")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
+                Spacer()
                 Text("チョキ")
+                    .padding(.bottom)
             }else{
                 Image("pa")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
+                Spacer()
                 Text("パー")
+                    .padding(.bottom)
             }
             
             Button("じゃんけんをする!") {
@@ -39,9 +46,13 @@ struct ContentView: View {
                 repeat{
                     newAnswerNumber = Int.random(in: 1...3)
                 }while answerNumber == newAnswerNumber
-                
                 answerNumber = newAnswerNumber
             }
+            .frame(maxWidth: .infinity)
+            .frame(height: 100)
+            .font(.title)
+            .background(Color.blue)
+            .foregroundColor(Color.white)
         }
     }
 }
